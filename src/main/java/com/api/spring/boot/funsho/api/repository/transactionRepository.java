4f3f358a4b892955e9f89f5240858ba25e318928
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-06-06 23:33:42 +0530

    deletion bug fix

--- src/main/java/com/api/spring/boot/funsho/api/repository/transactionRepository.java
+++ src/main/java/com/api/spring/boot/funsho/api/repository/transactionRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 public interface transactionRepository extends JpaRepository<transaction,Long>{
 
     @Modifying
-    @Query("delete from transaction where wallet_id = ?1")
+    @Query(value="delete from transaction where wallet_id = ?1",
+    nativeQuery = true)
     void deleteByWalletId(Long walletId);
     
 }
