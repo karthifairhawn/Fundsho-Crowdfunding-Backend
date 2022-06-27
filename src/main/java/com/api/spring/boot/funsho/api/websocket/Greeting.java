70cc4da597fb095542f48c992b4179890f73593f
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-06-27 12:19:54 +0530

    added Web socket

---
+++ src/main/java/com/api/spring/boot/funsho/api/websocket/Greeting.java
@@ -0,0 +1,14 @@
+package com.api.spring.boot.funsho.api.websocket;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private String message;
+}
---
+++ src/main/java/com/api/spring/boot/funsho/api/websocket/HelloMessage.java
@@ -0,0 +1,13 @@
+package com.api.spring.boot.funsho.api.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+
+    private String name;
+}
