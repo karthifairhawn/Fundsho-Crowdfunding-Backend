f050cb00c3fefcc1aa6e73c2f30591c74c858262
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-06-28 15:00:24 +0530

    Removed Websocket due to bug

--- src/main/java/com/api/spring/boot/funsho/api/resource/Miscellaneous.java
+++ src/main/java/com/api/spring/boot/funsho/api/resource/Miscellaneous.java
@@ -6,13 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
