29e9e1ddbefdd1b1c08ea17c8bdde94b647269d9
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-07-03 19:13:07 +0530

    auto generation of contact ID

--- src/main/java/com/api/spring/boot/funsho/api/entity/contact/Contact.java
+++ src/main/java/com/api/spring/boot/funsho/api/entity/contact/Contact.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class Contact {
     
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     Long contactId;
 
     String name;
