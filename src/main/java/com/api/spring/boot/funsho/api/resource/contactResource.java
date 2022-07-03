11329d07ae50c157ea11a323161eae938b174dd4
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-07-03 19:10:13 +0530

    resource bug fix

--- src/main/java/com/api/spring/boot/funsho/api/resource/contactResource.java
+++ src/main/java/com/api/spring/boot/funsho/api/resource/contactResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class contactResource {
     }
 
     @PostMapping("/contactinfo")
-    public Contact saveContactInfo(Contact contact) {
+    public Contact saveContactInfo(@RequestBody Contact contact) {
         contact.setTimestamp(new Date());
         return contactRepository.save(contact);
     }
